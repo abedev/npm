3d55e41f25dac626593446534a8daa7005d7496f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-01 12:26:56 -0700

    more model options

--- src/npm/sequelize/ModelOptions.hx
+++ src/npm/sequelize/ModelOptions.hx
@@ -5,5 +5,27 @@ typedef ModelOptions = {
   ?timestamps : Bool,
   ?underscored : Bool,
   ?freezeTableName : Bool,
-  ?tableName : String
+  ?tableName : String,
+  ?name : {
+    ?singular : String -> String,
+    ?plural : String -> String
+  },
+  ?indexes : Array<IndexOptions>
+}
+
+typedef IndexOptions = {
+  ?name : String,
+  ?type : String,
+  ?methods : String,
+  ?unique : Bool,
+  ?concurrently : Bool,
+  ?fields : Array<IndexFieldOptions>
+}
+
+// TODO are these optionals
+typedef IndexFieldOptions = {
+  attribute : String,
+  length : Int,
+  order : Dynamic, // TODO ?
+  collate : Dynamic, // TODO ?
 }
