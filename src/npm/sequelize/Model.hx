9a5765468aa212b7ae8eec420eb93c7fd40d6896
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-01 14:46:37 -0700

    more Model signatures and notes

--- src/npm/sequelize/Model.hx
+++ src/npm/sequelize/Model.hx
@@ -5,24 +5,56 @@ import thx.core.Nil;
 
 extern class Model<T : ModelInstance<T>> {
   function sync() : Promise<Model<T>>;
-  function drop() : Promise<Nil>; // TODO Nil?
-  function find(query : EitherType<Int, QueryOptions>) : Promise<T>;
-  function findAll(?query : QueryOptions) : Promise<Array<T>>;
-  function findOne(query : EitherType<Int, QueryOptions>) : Promise<T>;
+  function drop(?options : DropOptions) : Promise<Nil>; // TODO Nil?
+  function find(query : EitherType<Int, Query>) : Promise<T>;
+  //findAll([options], [queryOptions]) -> Promise<Array<Instance>>
+  function findAll(?query : Query, ?options : QueryOptions) : Promise<Array<T>>; // TODO options
+  function findOne(?query : EitherType<Int, Query>, ?options : QueryOptions) : Promise<T>;
+  function count(?query : Query) : Promise<Int>;
+  //findAndCount([findOptions], [queryOptions]) -> Promise<Object>
+  //max(field, [options]) -> Promise<Any>
+  //min(field, [options]) -> Promise<Any>
+  //sum(field, [options]) -> Promise<Number>
+  //build(values, [options]) -> Instance
+  //create(values, [options]) -> Promise<Instance>
+  //findOrInitialize -> Promise<Instance>
+  //findOrCreate(options, [queryOptions]) -> Promise<Instance|created>
+  //upsert(values, [options]) -> Promise<created>
+  //bulkCreate(records, [options]) -> Promise<Array<Instance>>
+  //update(values, options) -> Promise<Array<affectedCount|affectedRows>>
+  //describe() -> Promise
 
-  inline function findAttributes<TAt>(query : EitherType<Int, QueryOptions>) : Promise<TAt>
+  inline function findAttributes<TAt>(query : EitherType<Int, Query>) : Promise<TAt>
     return cast find(query);
-  inline function findOneAttributes<TAt>(query : EitherType<Int, QueryOptions>) : Promise<TAt>
+  inline function findOneAttributes<TAt>(query : EitherType<Int, Query>) : Promise<TAt>
     return cast findOne(query);
 
   function build(?defaults : {}) : T;
 
+  //destroy(options) -> Promise<undefined>
   function destroy(options : DestroyOptions) : Promise<Nil>;
+
+  //aggregate(field, aggregateFunction, [options]) -> Promise<options.dataType|object>
+
+  //schema(schema, [options]) -> this
+  //getTableName(options) -> String|Object
+  //scope(options*) -> Model
+}
+
+typedef Query = {
+  ?where : Dynamic,
+  ?attributes : Array<String>,
+  ?paranoid : Bool,
+  //?include : Array<Object | Model> TODO
+  //?order : String | Array | Sequelize.fn
+  ?limit : Int,
+  ?offset : Int,
+  //?transaction : Transaction
+
 }
 
 typedef QueryOptions = {
-  ?where : Dynamic,
-  ?attributes : Array<Dynamic>
+  lock : String
 }
 
 typedef DestroyOptions = {
