9ed5e0e928aab30b135c8f144e63da8b9801bf66
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-01 12:27:10 -0700

    more sequelize options

--- src/npm/Sequelize.hx
+++ src/npm/Sequelize.hx
@@ -7,6 +7,9 @@ extern class Sequelize {
   @:overload(function(connectionstring : String, ?options : DatabaseOptions) : Void {})
   function new(database : String, username : String, password : String, ?options : DatabaseOptions) : Void;
   function define<T : ModelInstance<T>>(modelName : String, attributes : Dynamic<ColumnOptions>, ?options : ModelOptions) : Model<T>;
+  function getDialect() : String;
+  //function getQueryInterface() : QueryInterface;
+  //function getMigrator(?options : MigratorOptions, ?force : Bool) : Migrator;
 
   static function STRING() : DataType;
   static function INTEGER() : DataType;
