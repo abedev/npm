b6f226aa7b145468a442cbd24402bc6aef87ac52
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-01 17:55:09 -0700

    more types in ModelInstance

--- src/npm/sequelize/ModelInstance.hx
+++ src/npm/sequelize/ModelInstance.hx
@@ -2,17 +2,17 @@ package npm.sequelize;
 
 import thx.core.Nil;
 
-extern class ModelInstance<T : ModelInstance<T>> {
+extern class ModelInstance<T : > {
   var createdAt : Date;
   var updatedAt : Date;
 
-  function isNewRecord() : Bool;
+  var isNewRecord(default, null) : Bool;
   function Model() : Model<T>;
-  function sequelize() : Sequelize;
-  var isDeleted : Bool;
-  function values() : Dynamic;
-  function isDirty() : Bool;
-  function primaryKeyValues() : Dynamic;
+  var sequelize(default, null) : Sequelize;
+  var isDeleted(default, null) : Bool;
+  var values(default, null) : Dynamic;
+  var isDirty(default, null) : Bool;
+  var primaryKeyValues(default, null) : Dynamic;
   function getDataValue(key : String) : Dynamic;
   function setDataValue(key : String, value : Dynamic) : Void;
   @:overload(function(?options : { plain : Bool }) : Dynamic {})
@@ -21,9 +21,16 @@ extern class ModelInstance<T : ModelInstance<T>> {
   @:overload(function() : Array<Bool> {})
   function changed(key : String) : Bool;
   function previous(key : String) : Dynamic;
-
+  //validate([options]) -> Promise<undefined|Errors.ValidationError> // TODO
   function save(?options : { ?fields : {}, ?silent : Bool, ?validate : Bool, ?transaction : Dynamic }) : Promise<T>; // either ValidationError or ModelInstance
   function reload() : Promise<T>;
-  function update(updated : Dynamic, options : Dynamic) : Promise<T>;
+  function update(updated : Dynamic, options : Dynamic) : Promise<T>; // TODO
+  //destroy([options={}]) -> Promise<undefined> // TODO
   function destroy() : Promise<Nil>;
+  //restore([options={}]) -> Promise<undefined> // TODO
+  //increment(fields, [options]) -> Promise<this> // TODO
+  //decrement(fields, [options]) -> Promise // TODO
+  function equals(other : ModelInstance<T>) : Bool;
+  function equalsOneOf(others : Array<ModelInstance<T>>) : Bool;
+  function toJSON() : {};
 }
