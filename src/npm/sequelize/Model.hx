46a245bf8d103a13488f3ed890cdd2e68ee989b7
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-03-03 10:43:21 -0700

    Unable Sequelize's model update method

--- src/npm/sequelize/Model.hx
+++ src/npm/sequelize/Model.hx
@@ -21,7 +21,7 @@ extern class Model<T : ModelInstance<T>> {
   //findOrCreate(options, [queryOptions]) -> Promise<Instance|created>
   //upsert(values, [options]) -> Promise<created>
   //bulkCreate(records, [options]) -> Promise<Array<Instance>>
-  //update(values, options) -> Promise<Array<affectedCount|affectedRows>>
+  function update(values : {}, options : {}) :Promise<Array<Int>>
   //describe() -> Promise
 
   inline function findAttributes<TAt>(query : EitherType<Int, Query>) : Promise<TAt>
