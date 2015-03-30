42c7197d4bfe6c28a1e62f9b83aefc855519a998
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-30 17:26:45 -0600

    converted tabs to spaces

--- src/npm/moment/Duration.hx
+++ src/npm/moment/Duration.hx
@@ -1,21 +1,21 @@
 package npm.moment;
 
 extern class Duration {
-	function abs() : Float;
-	function weeks() : Int;
-	function valueOf() : Float;
-	function humanize(withSuffix : Bool) : String;
-	@:overload(function(moment : Moment) : Duration {})
-	function add(value : Float, unit : String) : Duration;
-	@:overload(function(moment : Moment) : Duration {})
-	function subtract(value : Float, unit : String) : Duration;
-	function get(units : String) : Float;
-	function as(units : String) : Float;
+  function abs() : Float;
+  function weeks() : Int;
+  function valueOf() : Float;
+  function humanize(withSuffix : Bool) : String;
+  @:overload(function(moment : Moment) : Duration {})
+  function add(value : Float, unit : String) : Duration;
+  @:overload(function(moment : Moment) : Duration {})
+  function subtract(value : Float, unit : String) : Duration;
+  function get(units : String) : Float;
+  function as(units : String) : Float;
 
-	// locale()
-	function toISOString() : String;
-	// function localeData()
-	function toJSON() : String;
+  // locale()
+  function toISOString() : String;
+  // function localeData()
+  function toJSON() : String;
 
   function asMilliseconds() : Float;
   function asSeconds() : Float;
