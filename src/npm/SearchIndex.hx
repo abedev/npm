20479c64bcd25ef714345a8abe47b754e3690d3d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 17:15:00 -0600

    adding options

--- src/npm/SearchIndex.hx
+++ src/npm/SearchIndex.hx
@@ -2,5 +2,5 @@ package npm;
 
 @:jsRequire("search-index")
 extern class SearchIndex {
-	
+	@:selfCall new(options : Options) : Void;
 }
---
+++ src/npm/searchindex/Options.hx
@@ -0,0 +1,5 @@
+package npm.searchindex;
+
+typedef Options = {
+	
+}
