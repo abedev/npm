bd6ef5e91c35aaa6dfb842cd9af4283291b18b4e
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:04:32 -0600

    release

--- submit.sh
+++ submit.sh
@@ -3,5 +3,5 @@ rm npm.zip 2> /dev/null
 haxe build.hxml
 rm -f docs/offline.zip
 zip -r npm.zip hxml src test docs extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
-./generate.sh
 haxelib submit npm.zip
+./generate.sh
