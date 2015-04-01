1cc3098d3aefea5b35542b8f029ea5587e29ee2b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 19:57:36 -0600

    added documentation zip

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 haxelib run dox -o docs -i xml/js.xml --title "NPM for Haxe" --toplevel-package "npm" -theme flambe
 git archive -o docs/offline.zip HEAD docs
+rm -f docs/offline.zip
+zip -r docs/offline.zip docs -x "*/\.*"
