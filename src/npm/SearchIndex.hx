33c2588ac491ecd8fd0876209cb1bb367fd05359
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 18:59:49 -0600

    added remaining methods to SearchIndex

--- src/npm/SearchIndex.hx
+++ src/npm/SearchIndex.hx
@@ -1,6 +1,26 @@
 package npm;
 
+import npm.common.Callback;
+import npm.searchindex.*;
+
 @:jsRequire("search-index")
 extern class SearchIndex {
 	@:selfCall new(options : Options) : Void;
+
+	// TODO check return types for all functions
+	@:overload(function(options : AddOptions, data : Data, callback : Callback0) : Void {})
+	function add(options : AddOptions, data : Array<Data>, callback : Callback0) : Void;
+
+	function del(docID : String, callback : Callback0) : Void;
+	function get<T : Data>(docID : String, callback : Callback<T>) : Void;
+	function search<T : Data>(query : Query, callback : Callback<Array<T>>) : Void;
+	function match(beginsWith : String, callback : Callback<Array<String>>) : Void;
+	// TODO check that message type is String
+	function tellMeAboutMySearchIndex(callback : String -> Void) : Void;
+	function empty(callback : Callback<0>) : Void;
+#if hxnodejs
+	function snapshot(callback : js.node.stream.Readable) : Void;
+	// TODO check that message type is String
+	function replicate(source : js.node.stream.Readable, callback : String -> Void) : Void;
+#end
 }
