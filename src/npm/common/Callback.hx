bdc85833d670215b3c9f40a1f50c8ae2e0058769
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 19:12:57 -0600

    fixed callback

--- src/npm/common/Callback.hx
+++ src/npm/common/Callback.hx
@@ -1,6 +1,6 @@
 package npm.common;
 
-typedef Callback0<T> = js.Error -> Void;
+typedef Callback0 = js.Error -> Void;
 typedef Callback<T> = js.Error -> T -> Void;
 typedef Callback1<T> = Callback<T>;
 typedef Callback2<T1, T2> = js.Error -> T1 -> T2 -> Void;
