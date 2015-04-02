901f71c784c499a4179f241810203d654198a8b2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-02 17:42:10 -0600

    completed QueryOptions

--- src/npm/sequelize/QueryOptions.hx
+++ src/npm/sequelize/QueryOptions.hx
@@ -1,4 +1,12 @@
 package npm.sequelize;
 
+import haxe.extern.EitherType;
+
 typedef QueryOptions = {
+	?transaction : Transaction,
+	?type : String,
+	?nest : Bool,
+	?plain : Bool,
+	?useMaster : Bool,
+	?replacements : EitherType<Dynamic<String>, Array<String>>
 }
