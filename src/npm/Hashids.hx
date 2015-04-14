c5d9e9c89354b37c5ee66b2dde76129f6fd8f06a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-14 16:58:42 -0600

    basic methods

--- src/npm/Hashids.hx
+++ src/npm/Hashids.hx
@@ -1,6 +1,12 @@
 package npm;
 
+import haxe.extern.Rest;
+
 @:jsRequire("hashids")
 extern class Hashids {
-  
+  function new(salt : String) : Void;
+
+  @:overload(function(value : Int) : String {})
+  function encode(values : Rest<Int>) : String;
+  function decode(value : String) : Array<Int>;
 }
