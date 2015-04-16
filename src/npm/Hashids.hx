6b52de9edb22327d4244d5c09f1910e29ba0680b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-15 23:29:26 -0600

    split constructor of Hashids

--- src/npm/Hashids.hx
+++ src/npm/Hashids.hx
@@ -4,12 +4,18 @@ import haxe.extern.Rest;
 
 @:jsRequire("hashids")
 extern class Hashids {
-  function new(salt : String, ?minlength : Int, ?alphabet : String) : Void;
+  @:overload(function(salt : String, minlength : Int, alphabet : String) : String {})
+  @:overload(function(salt : String, alphabet : String) : String {})
+  @:overload(function(salt : String, minlength : Int) : String {})
+  function new(salt : String) : Void;
 
   @:overload(function(values : Array<Int>) : String {})
   @:overload(function(values : Rest<Int>) : String {})
   function encode(value : Int) : String;
+
   function encodeHex(value : String) : String;
+
   function decode(value : String) : Array<Int>;
+
   function decodeHex(value : String) : String;
 }
