a1b4e51018d7e6b39dd424a82ef35fa391edd269
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-21 15:28:53 -0600

    added missing sign options

--- src/npm/awscloudfrontsign/SignOptions.hx
+++ src/npm/awscloudfrontsign/SignOptions.hx
@@ -1,5 +1,10 @@
 package npm.awscloudfrontsign;
 
+import haxe.extern.EitherType;
+
 typedef SignOptions = {
-  
+  expireTime : EitherType<Float, EitherType<Date, npm.Moment>>,
+  keypairId : String,
+  ?privateKeyString : String,
+  ?privateKeyPath : String
 }
