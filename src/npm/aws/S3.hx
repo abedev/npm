8ad210df6fd239a4482714dba6d92dadbeb7e05e
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-22 14:55:17 -0600

    added S3.deleteObject

--- src/npm/aws/S3.hx
+++ src/npm/aws/S3.hx
@@ -9,4 +9,6 @@ extern class S3 {
   function putObject(params : PutParams, ?optons : {}, callback : Callback<Dynamic>) : Void;
   // TODO check return type, params, options, and callback type
   function upload(params : PutParams, ?optons : {}, callback : Callback<Dynamic>) : Void;
+  // TODO check return type, options, and callback type
+  function deleteObject(params : DeletetParams, callback : Callback<Dynamic>) : Void;
 }
