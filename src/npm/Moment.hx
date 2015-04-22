d4ffe88ebaef88d49a22107174eed5b15ceceb4d
Andy White <greatwhitehope@gmail.com>
2015-04-22 10:32:45 -0600

    Adjust utcOffset overloads

--- src/npm/Moment.hx
+++ src/npm/Moment.hx
@@ -89,9 +89,10 @@ extern class Moment {
   // function isoWeekYear(?Number) : Moment;
   // funciton weeksInYear() : Moment;
 
-  @:overload(function(?setTo : String) : Moment {})
-  @:overload(function(?setTo : Float) : Moment {})
-  function utcOffset(?setTo : Int) : Moment;
+  @:overload(function(setTo : String) : Moment {})
+  @:overload(function(setTo : Float) : Moment {})
+  @:overload(function(setTo : Int) : Moment {})
+  function utcOffset() : Int;
 
   @:overload(function(duration : String) : Moment {})
   @:overload(function(options : Dynamic<Float>) : Moment {})
