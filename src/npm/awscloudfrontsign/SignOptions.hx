9d4c3bc94d8e0ed7e0f2a191bcce9176b7f7ff68
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-22 23:34:53 -0600

    make expireTime optional (30 secs is default)

--- src/npm/awscloudfrontsign/SignOptions.hx
+++ src/npm/awscloudfrontsign/SignOptions.hx
@@ -3,7 +3,7 @@ package npm.awscloudfrontsign;
 import haxe.extern.EitherType;
 
 typedef SignOptions = {
-  expireTime : EitherType<Float, EitherType<Date, npm.Moment>>,
+  ?expireTime : EitherType<Float, EitherType<Date, npm.Moment>>,
   keypairId : String,
   ?privateKeyString : String,
   ?privateKeyPath : String
