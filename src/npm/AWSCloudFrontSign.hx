c93ecc2687e8312492d31af58aa38df22b596579
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-22 23:34:32 -0600

    typo

--- src/npm/AWSCloudFrontSign.hx
+++ src/npm/AWSCloudFrontSign.hx
@@ -3,7 +3,7 @@ package npm;
 import npm.awscloudfrontsign.*;
 
 @:jsRequire("aws-cloudfront-sign")
-extern class AWSCloudfrontSign {
+extern class AWSCloudFrontSign {
   static function getSignedUrl(url : String, options : SignOptions) : String;
   static function getSignedRTMPUrl(domainName : String, s3key : String, options : SignOptions) : SignedRTMPUrl;
 }
