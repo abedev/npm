4f5d952fbd32cfbf421b037ae5fe458e3f9bbf63
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-30 17:00:22 -0600

    added parse to content disposition

--- src/npm/ContentDisposition.hx
+++ src/npm/ContentDisposition.hx
@@ -5,4 +5,5 @@ import npm.contentdisposition.*;
 @:jsRequire("content-disposition")
 extern class ContentDisposition {
   @:selfCall static function get(filename : String, ?options : Options) : String;
+  static function parse(value : String) : { type : DispositionType, parameters : Dynamic<String> };
 }
