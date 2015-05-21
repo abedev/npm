3665f4ccf076c72de62dae277b45709a674f93f4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-21 10:31:12 -0600

    completed GetObjectOptions

--- src/npm/aws/GetObjectOptions.hx
+++ src/npm/aws/GetObjectOptions.hx
@@ -1,5 +1,24 @@
 package npm.aws;
 
-typedef GetObjectOptions = {
+import haxe.extern.EitherType;
 
+typedef GetObjectOptions = {
+  Bucket : String,
+  Key : String,
+  ?IfMatch : String,
+  ?IfModifiedSince : EitherType<Date, EitherType<String, Float>>,
+  ?IfNoneMatch : String,
+  ?IfUnmodifiedSince : EitherType<Date, EitherType<String, Float>>,
+  ?Range : String,
+  ?RequestPayer : String,
+  ?ResponseCacheControl : String,
+  ?ResponseContentDisposition : String,
+  ?ResponseContentEncoding : String,
+  ?ResponseContentLanguage : String,
+  ?ResponseContentType : String,
+  ?ResponseExpires : EitherType<Date, EitherType<String, Float>>,
+  ?SSECustomerAlgorithm : String,
+  ?SSECustomerKey : String,
+  ?SSECustomerKeyMD5 : String,
+  ?VersionId : String
 }
