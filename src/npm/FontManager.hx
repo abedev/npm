108ad86284a73132c640911a1e652579826dd6b7
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-09-03 10:52:35 -0600

    added FontDescription and FontManager.getAvailableFonts

--- src/npm/FontManager.hx
+++ src/npm/FontManager.hx
@@ -1,6 +1,8 @@
 package npm;
 
+import npm.fontmanager.*;
+
 @:jsRequire("font-manager")
 extern class FontManager {
-
+  static function getAvailableFonts() : Array<FontDescription>;
 }
---
+++ src/npm/fontmanager/FontDescription.hx
@@ -0,0 +1,12 @@
+package npm.fontmanager;
+
+typedef FontDescription = {
+  path : String,
+  postscriptName : String,
+  family : String,
+  style : String,
+  weight : Float,
+  width : Float,
+  italic : Bool,
+  monospace : Bool
+}
