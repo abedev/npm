a3db4e60710a26cfe85eb195dba764c461fbd9be
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-09 17:37:42 -0600

    fixed event case

--- src/npm/engineio/Socket.hx
+++ src/npm/engineio/Socket.hx
@@ -13,31 +13,32 @@ extern class Socket extends js.node.events.EventEmitter<Socket> {
   // TODO type appropriately
   var transport(default, null) : Dynamic;
 
-  inline function onclose(callback : Callback2<String, Dynamic>) : Socket
+  inline function onClose(callback : Callback2<String, Dynamic>) : Socket
     return this.on("close", callback);
 
-  inline function onmessage(callback : Callback<EitherType<String, js.node.Buffer>>) : Socket
+  inline function onMessage(callback : Callback<EitherType<String, js.node.Buffer>>) : Socket
     return this.on("message", callback);
 
-  inline function onerror(callback : Callback0) : Socket
+  inline function onError(callback : Callback0) : Socket
     return this.on("error", callback);
 
   // TODO is Array<Int> correct?
-  inline function onflush(callback : Callback<Array<Int>>) : Socket
+  inline function onFlush(callback : Callback<Array<Int>>) : Socket
     return this.on("flush", callback);
 
-  inline function ondrain(callback : Callback0) : Socket
+  inline function onDrain(callback : Callback0) : Socket
     return this.on("drain", callback);
 
   // TODO is Callback2<String, String> correct?
-  inline function onpacket(callback : Callback2<String, String>) : Socket
+  inline function onPacket(callback : Callback2<String, String>) : Socket
     return this.on("packet", callback);
 
   // TODO is Callback2<String, String> correct?
-  inline function onpacketCreate(callback : Callback2<String, String>) : Socket
+  inline function onPacketCreate(callback : Callback2<String, String>) : Socket
     return this.on("packetCreate", callback);
 
   @:override(function(message : MessageType, options : SendOptions, callback : Callback0) : Socket {})
+  @:override(function(message : MessageType, callback : Callback0) : Socket {})
   @:override(function(message : MessageType, options : SendOptions) : Socket {})
   function send(message : MessageType) : Socket;
 
