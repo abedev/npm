f47768c5046dfb6816a3e37bca3bbe8945941b2d
Andy White <greatwhitehope@gmail.com>
2015-10-26 14:15:28 -0600

    Make forever options optional

--- src/npm/ForeverMonitor.hx
+++ src/npm/ForeverMonitor.hx
@@ -4,7 +4,7 @@ import npm.forever.ForeverOptions;
 
 @:jsRequire("forever-monitor", "Monitor")
 extern class ForeverMonitor {
-  function new(scriptPath : String, options : ForeverOptions);
+  function new(scriptPath : String, ?options : ForeverOptions);
 
   function on(event : String, callback : ?Dynamic -> Void): Void;
 
