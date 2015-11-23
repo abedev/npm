e4deb5dfda3c2feb28cbf1a86061f3db23f1dcc9
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-11-23 16:25:43 -0700

    Don't try to tell the compiler about objects

--- src/npm/lf/schema/Table.hx
+++ src/npm/lf/schema/Table.hx
@@ -4,6 +4,6 @@ interface Table {
   function as(name : String) : Table;
 
    // value is an object with keys matching column names
-  function createRow(value : Dynamic<Dynamic>) : npm.lf.Row;
+  function createRow(value : Dynamic) : npm.lf.Row;
   function getName() : String;
 }
