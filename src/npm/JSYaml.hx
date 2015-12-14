41d447244fdcf1af9ed2a30fd0ba1a01f371b5a5
Andy White <greatwhitehope@gmail.com>
2015-12-14 10:26:46 -0700

    Make js-yaml functions static

--- src/npm/JSYaml.hx
+++ src/npm/JSYaml.hx
@@ -5,10 +5,10 @@ import npm.jsyaml.*;
 #if norequire @:native('jsyaml')
 #else @:jsRequire('js-yaml') #end
 extern class JSYaml {
-  public function safeLoad(input : String, ?options : LoadOptions) : {};
-  public function load(input : String, ?options : LoadOptions) : {};
-  public function safeLoadAll(input : String, iterator : String -> Void, ?options : LoadOptions) : {};
-  public function loadAll(input : String, iterator : String -> Void, ?options : LoadOptions) : {};
-  public function safeDump(input : {}, ?options : DumpOptions) : String;
-  public function dump(input : {}, ?options : DumpOptions) : String;
+  public static function safeLoad(input : String, ?options : LoadOptions) : {};
+  public static function load(input : String, ?options : LoadOptions) : {};
+  public static function safeLoadAll(input : String, iterator : String -> Void, ?options : LoadOptions) : {};
+  public static function loadAll(input : String, iterator : String -> Void, ?options : LoadOptions) : {};
+  public static function safeDump(input : {}, ?options : DumpOptions) : String;
+  public static function dump(input : {}, ?options : DumpOptions) : String;
 }
