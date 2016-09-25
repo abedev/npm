684584835fcc0ae137b59660a7069ac8fc12e140
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-09-24 20:55:15 -0600

    added return type for save

--- src/npm/arango/DocumentCollection.hx
+++ src/npm/arango/DocumentCollection.hx
@@ -1,8 +1,13 @@
 package npm.arango;
 
 import js.Promise;
+import haxe.extern.EitherType;
 
 extern class DocumentCollection extends Collection<DocumentCollection> {
   function document(documentHandle : String) : Promise<Null<Dynamic>>;
-  function save(data : Dynamic) : Promise<{}>; // TODO
+  function save(data : Dynamic) : Promise<{
+    _id: String,
+    _key: EitherType<String, Int>,
+    _rev: Int
+  }>;
 }
