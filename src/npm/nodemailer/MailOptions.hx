6f1802ee849269b032817063f161178ac153f1f1
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-10-24 11:36:31 -0600

    indentation and fixes

--- src/npm/nodemailer/MailOptions.hx
+++ src/npm/nodemailer/MailOptions.hx
@@ -3,22 +3,22 @@ package npm.nodemailer;
 import haxe.extern.EitherType;
 
 typedef MailOptions = {
-	from : EitherType<String, Email>,
-	?sender : EitherType<String, Email>,
-	to : EitherType<String, Email>,
-	?cc : EitherType<String, Email>,
-	?bcc : EitherType<String, Array<Email>>,
-	?replyTo : EitherType<String, Email>,
-	?inReplyTo : String,
-	?references : EitherType<String, Array<String>>,
-	subject : String,
-	text : String,
-	?html : String,
-	?headers : EitherType<Dynamic<String>, Array<{key : String, value : String}>>,
-	?attachments: Array<Attachment>,
-	// alternatives
-	// envelope
-	?messageId : String,
-	?date : Date,
-	?encodint : String
+  from : Email,
+  ?sender : Email,
+  to : Email,
+  ?cc : Email,
+  ?bcc : EitherType<String, Array<Email>>,
+  ?replyTo : Email,
+  ?inReplyTo : String,
+  ?references : EitherType<String, Array<String>>,
+  subject : String,
+  text : String,
+  ?html : String,
+  ?headers : EitherType<Dynamic<String>, Array<{key : String, value : String}>>,
+  ?attachments: Array<Attachment>,
+  // alternatives
+  // envelope
+  ?messageId : String,
+  ?date : Date,
+  ?encoding : String
 }
