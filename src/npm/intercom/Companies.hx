9d7d53acfc5b070e5057c4f5198aaeb94c6531bf
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-20 10:35:35 -0700

    no create for Companies

--- src/npm/intercom/Companies.hx
+++ src/npm/intercom/Companies.hx
@@ -3,7 +3,6 @@ package npm.intercom;
 import js.Promise;
 
 extern class Companies {
-  public function create(options: CompanyCreateOptions): Promise<Dynamic>; // TODO return type
   public function update(options: CompanyCreateOptions): Promise<Dynamic>; // TODO return type
   public function list(): Promise<Array<Company>>; // TODO return type
   public function listBy(options: {?tag_id: String, ?segment: String}): Promise<Array<Company>>; // TODO return type
