1136a60f8cd1d93ac447d83a16c8b85ecbcc6545
Franco Ponticelli <franco.ponticelli@gmail.com>
2017-04-04 15:56:10 -0600

    try to make twilio work

--- src/npm/twilio/RestClient.hx
+++ src/npm/twilio/RestClient.hx
@@ -2,7 +2,6 @@ package npm.twilio;
 
 @:jsRequire("twilio", "RestClient")
 extern class RestClient {
-  public var sms(default, null): { messages: Messages };
-
+  public var messages(default, null): Messages;
   public function new(accountSid: String, authToken: String) : Void;
 }
