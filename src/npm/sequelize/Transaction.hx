0a6796fccba34403789682dddcd88c407d1a43f4
Andy White <greatwhitehope@gmail.com>
2017-07-28 10:54:32 -0600

    Update sequelize.TransactionOptions

--- src/npm/sequelize/Transaction.hx
+++ src/npm/sequelize/Transaction.hx
@@ -1,25 +1,15 @@
 package npm.sequelize;
 
+import npm.sequelize.TransactionOptions;
+
 import js.Promise;
 
-typedef ISOLATION_LEVELS = {
-  READ_UNCOMMITTED : String,
-  READ_COMMITTED : String,
-  REPEATABLE_READ : String,
-  SERIALIZABLE : String
-};
-
-typedef TYPES = {
-  DEFERRED : String,
-  IMMEDIATE : String,
-  EXCLUSIVE : String
-};
-
+@:jsRequire("sequelize", "Transaction")
 extern class Transaction {
   public static var ISOLATION_LEVELS : ISOLATION_LEVELS;
   public static var TYPES : TYPES;
 
-  function new(sequelize : Sequelize, options: TransactionOptions);
+  function new(sequelize : Sequelize, ?options: TransactionOptions);
 
   function commit() : Promise<Void>;
   function rollback() : Promise<Transaction>;
--- src/npm/sequelize/TransactionOptions.hx
+++ src/npm/sequelize/TransactionOptions.hx
@@ -1,5 +1,18 @@
 package npm.sequelize;
 
+typedef ISOLATION_LEVELS = {
+  READ_UNCOMMITTED : String,
+  READ_COMMITTED : String,
+  REPEATABLE_READ : String,
+  SERIALIZABLE : String
+};
+
+typedef TYPES = {
+  DEFERRED : String,
+  IMMEDIATE : String,
+  EXCLUSIVE : String
+};
+
 typedef TransactionOptions = {
   ?autocommit: Bool,
   ?type: String,
