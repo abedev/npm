9c54a9fabea2226475808d7b2f4534667b6fd5cd
Franco Ponticelli <franco.ponticelli@gmail.com>
2017-10-01 12:11:03 -0600

    oopsie

--- src/npm/Sha1.hx
+++ src/npm/Sha1.hx
@@ -3,7 +3,7 @@ package npm;
 @:jsRequire("sha1")
 extern class Sha1 {
   @:selfCall
-  @:overload(function(buffer : Array<Int>) : String)
-  @:overload(function(buffer : js.node.Buffer) : String)
+  @:overload(function(buffer : Array<Int>) : String {})
+  @:overload(function(buffer : js.node.Buffer) : String {})
   static function hash(value : String) : String;
 }
